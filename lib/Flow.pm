af8c7c8ad2619071d810b9bc568d991c7eecb6f5
Zag <github.send@zag.ru>
2015-03-01 13:53:22 +0300

    fix ver

--- lib/Flow.pm
+++ lib/Flow.pm
@@ -49,7 +49,7 @@ use warnings;
 use Exporter;
 our @ISA    = qw(Exporter);
 our @EXPORT = qw(create_flow);
-our $VERSION = 1.0;
+our $VERSION = '1.0';
 use constant MODS_MAP => {
     Splice  => 'Flow::Splice',
     Join    => 'Flow::Join',
